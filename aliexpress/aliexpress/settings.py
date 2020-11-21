5d754fd401150a2b5183ca394de168425e950bce
Deviska Nugroho <deviskanugroho1@gmail.com>
2020-11-21 16:17:22 +0700

    add project aliexpress

--- aliexpress/aliexpress/settings.py
+++ aliexpress/aliexpress/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for quotes_spider project
+# Scrapy settings for aliexpress project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'quotes_spider'
+BOT_NAME = 'aliexpress'
 
-SPIDER_MODULES = ['quotes_spider.spiders']
-NEWSPIDER_MODULE = 'quotes_spider.spiders'
+SPIDER_MODULES = ['aliexpress.spiders']
+NEWSPIDER_MODULE = 'aliexpress.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'quotes_spider (+http://www.yourdomain.com)'
+#USER_AGENT = 'aliexpress (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -45,7 +45,7 @@ ROBOTSTXT_OBEY = False
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'quotes_spider.middlewares.QuotesSpiderSpiderMiddleware': 543,
+#    'aliexpress.middlewares.AliexpressSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
@@ -69,9 +69,9 @@ HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'quotes_spider.pipelines.QuotesSpiderPipeline': 300,
-# }
+#ITEM_PIPELINES = {
+#    'aliexpress.pipelines.AliexpressPipeline': 300,
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
