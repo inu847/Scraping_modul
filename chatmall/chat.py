2df99e7f98208467e0aa7a531b6595d50468d430
Adin72978 <Adin72978@gmail.com>
2020-12-19 15:31:03 +0700

    automation wait

--- chatmall/chat.py
+++ chatmall/chat.py
@@ -1,41 +1,62 @@
 from selenium import webdriver
 from parsel import Selector
+from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 import parameters
 import csv
 
 def webchat(username):
-    writer = csv.writer(open(parameters.result_file, 'w'))
-    writer.writerow(['chat_from', 'chat_me', 'chat_buyer'])
+    # writer = csv.writer(open(parameters.result_file, 'w'))
+    # writer.writerow(['chat_from', 'chat_me', 'chat_buyer', 'product_name', 'product_price'])
 
     driver = webdriver.Chrome()
     driver.maximize_window()
+    driver.implicitly_wait(20)
+    # driver.minimize_window()
     driver.get('https://seller.shopee.co.id/webchat/conversations')
-    sleep(5)
+    driver.implicitly_wait(20)
 
     user = driver.find_element_by_xpath('//*[@autocomplete="username"]')
     user.send_keys(username)
-    sleep(3)
+    driver.implicitly_wait(20)
 
     passwd = driver.find_element_by_xpath('//*[@autocomplete="current-password"]')
     passwd.send_keys(parameters.password)
-    sleep(3)
+    driver.implicitly_wait(20)
+    
     driver.find_element_by_xpath('//*[@class="shopee-button shopee-button--primary shopee-button--large shopee-button--block"]').click()
-    sleep(5)
+    driver.implicitly_wait(20)
+
+    sel = Selector(text=driver.page_source)
+    error = driver.find_element_by_xpath('//*[@class="shopee-alert-title"]')
+    # note = sel.xpath('//*[@class="shopee-alert-title"]/text()').extract_first().strip()
+    if error:
+        print('Gagal Login cek username password')
+    else:
+        print(username + ' success logon')
 
     # API BUYER CHAT = https://seller.shopee.co.id/webchat/api/v1.2/conversations/329442619095935409/messages?shop_id=219568618&offset=0&limit=20&direction=older&_s=4&_uid=0-219572657&_v=4.7.0&csrf_token=nrrq%2Fz9YEeuWAsy7%2Fl4BQg%3D%3D
     # LEN CHAT BUYER
     buyers = driver.find_elements_by_xpath('//*[@class="z8iJb5JoTh "]')
     for buyer in buyers:
         buyer.click()
-        sleep(3)
+        driver.implicitly_wait(2)
+        
+        try:
+            driver.find_element_by_xpath('//*[@class="_2hgL8kwJb4E7a3ggdF9PMp"]').click()
+        except:
+            driver.find_element_by_xpath('//*[@class="ReactVirtualized__Grid__innerScrollContainer"]')
+            # driver.find_element_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]')
+            # driver.find_element_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]')
 
-        sel = Selector(text=driver.page_source)
-        chat_from = sel.xpath('//*[@class="_34iAGBNPqd"]/text()').extract_first()
-        chat_me = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]/text()').extract()
-        chat_buyer = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]/text()').extract()
-        # product_name = sel.xpath('//*[@class="_2d5PdwXD15 _1I-Gx-uQ_G"]/text()').extract_first()
-        # product_price = sel.xpath('//*[@class="_2tIduzSqLM _2rEqItiXyt"]/text()').extract_first().split('Rp ')[1]
+            sel = Selector(text=driver.page_source)
+            chat_from = sel.xpath('//*[@class="_34iAGBNPqd"]/text()').extract_first()
+            chat_me = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]/text()').extract()
+            chat_buyer = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]/text()').extract()
+            # product_name = sel.xpath('//*[@class="_2d5PdwXD15 _1I-Gx-uQ_G"]/text()').extract_first()
+            # product_price = sel.xpath('//*[@class="_2tIduzSqLM _2rEqItiXyt"]/text()').extract_first()
+
+       
 
         print('/n')
         print(chat_from)
@@ -45,7 +66,7 @@ def webchat(username):
         # print(product_price)
         print('/n')
 
-        writer.writerow([chat_from, chat_me, chat_buyer])
+        # writer.writerow([chat_from, chat_me, chat_buyer, product_name, product_price])
 
     driver.quit()
 
--- chatmall/parameters.py
+++ chatmall/parameters.py
@@ -1,11 +1,6 @@
 # DIISI USERNAME UNTUK LOGIN SHOPEE
-email = ['username/email1', 
-         'username/email2',
-         'username/email3',
-         'username/email4',
-         'username/email5',
-         'username/email6',
-         'username/email7',]
+email = ['nabilahsuryof9', 
+         'ayularstore12',]
             
-password = 'Fuadinugroho'
-result_file = 'chat.csv'
+password = 'Semogaberkah'
+result_file = 'output.csv'
