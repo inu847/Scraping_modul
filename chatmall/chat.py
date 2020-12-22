c6f5088bda9f25c3e689fc0fa129c05cc6b98ba3
Adin72978 <Adin72978@gmail.com>
2020-12-22 15:49:25 +0700

    scraping success

--- chatmall/chat.py
+++ chatmall/chat.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from parsel import Selector
 from selenium.webdriver.support.ui import WebDriverWait
-from time import sleep
 import parameters
 import csv
 
@@ -40,36 +39,47 @@ def webchat(username, password):
     print('/n')
     print(len(buyers))
     print('/n')
+    
     for buyer in buyers:
         buyer.click()
-        driver.implicitly_wait(0.1)
+        driver.implicitly_wait(2)
+        
+        sel = Selector(text=driver.page_source)
+        connection_error = sel.xpath('//*[@class="_2hgL8kwJb4E7a3ggdF9PMp"]')
+        if connection_error:
+            main()
 
-        driver.find_element_by_xpath('//*[@class="ReactVirtualized__Grid__innerScrollContainer"]')
-        # driver.find_element_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]')
-        # driver.find_element_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]')
-
+        try:
+            driver.find_elements_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]')
+            driver.implicitly_wait(2)
+            driver.find_elements_by_xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]')
+            driver.implicitly_wait(2)
+            driver.find_elements_by_xpath('//*[@class="_2d5PdwXD15 _1I-Gx-uQ_G"]')
+            driver.implicitly_wait(2)
+            driver.find_elements_by_xpath('//*[@class="_2tIduzSqLM _2rEqItiXyt"]')
+            driver.implicitly_wait(2)
+            driver.find_elements_by_xpath('//*[@class="_1gEytIy7qKccGA5xfdWQVH"]')
+            driver.implicitly_wait(2)
+        except:
+            pass
+        
         sel = Selector(text=driver.page_source)
         chat_from = sel.xpath('//*[@class="_34iAGBNPqd"]/text()').extract_first()
         chat_me = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl _3oQWa8rdLlo8Vgb5aY_iJC"]/text()').extract()
-        chat_buyer = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]/text()').extract()
-        # product_name = sel.xpath('//*[@class="_2d5PdwXD15 _1I-Gx-uQ_G"]/text()').extract_first()
-        # product_price = sel.xpath('//*[@class="_2tIduzSqLM _2rEqItiXyt"]/text()').extract_first()
-
-       
+        chat_buyer = sel.xpath('//pre[@class="_2Zb8khbVxMFlHDDD2kMhKl"]/text()/following-sibling::*[@class="_3fF9tZdS-P  "]/text()').extract()
+        product_name = sel.xpath('//*[@class="_2d5PdwXD15 _1I-Gx-uQ_G"]/text()').extract_first()
+        product_price = sel.xpath('//*[@class="_2tIduzSqLM _2rEqItiXyt"]/text()').extract_first()
+        url_image = sel.xpath('//a[@class="_1gEytIy7qKccGA5xfdWQVH"]/@href').extract_first()
+        mssg_timestamp = sel.xpath('//div/[@class="_3fF9tZdS-P  "]/text()').extract_first()
 
         print('/n')
         print(chat_from)
         print(chat_me)
         print(chat_buyer)
-        # print(product_name)
-        # print(product_price)
+        print(product_name)
+        print(product_price)
+        print(url_image)
         print('/n')
-
-        try:
-            driver.find_element_by_xpath('//*[@class="_2hgL8kwJb4E7a3ggdF9PMp"]').click()
-            driver.implicitly_wait(2)
-        except:
-            pass
 
         # writer.writerow([chat_from, chat_me, chat_buyer, product_name, product_price])
 
